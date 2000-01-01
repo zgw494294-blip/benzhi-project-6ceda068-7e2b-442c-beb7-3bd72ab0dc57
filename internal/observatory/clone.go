1c414cedb21fddc605c380c76e4231046170cb27
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Client result for 6ceda068-7e2b-442c-beb7-3bd72ab0dc57-bug-5

--- internal/observatory/clone.go
+++ internal/observatory/clone.go
@@ -8,7 +8,20 @@ func CloneAggregate(source Aggregate) Aggregate {
 	}
 	clone.Findings = make(map[string]ValidationFinding, len(source.Findings))
 	for id, finding := range source.Findings {
+		if finding.ReviewedAt != nil {
+			reviewedAt := *finding.ReviewedAt
+			finding.ReviewedAt = &reviewedAt
+		}
 		clone.Findings[id] = finding
+	}
+	if source.Manifest != nil {
+		manifest := *source.Manifest
+		manifest.Entries = append([]ManifestEntry(nil), source.Manifest.Entries...)
+		clone.Manifest = &manifest
+	}
+	if source.Credential != nil {
+		credential := *source.Credential
+		clone.Credential = &credential
 	}
 	return clone
 }
